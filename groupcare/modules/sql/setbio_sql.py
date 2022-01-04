0194f58ff8ad8e152e72dfc1610eb69318b7bff2
LEGEND-VIVEK <96050472+LEGEND-VIVEK@users.noreply.github.com>
2022-01-04 19:56:12 +0530

    Update setbio_sql.py

--- groupcare/modules/sql/setbio_sql.py
+++ groupcare/modules/sql/setbio_sql.py
@@ -1,5 +1,5 @@
 import threading
-from mrjoker.modules.sql import BASE, SESSION
+from groupcare.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, UnicodeText, String
 
 
