e206674bdc158d0f3aa1dd17d16661c01ecdf7fe
LEGEND-VIVEK <96050472+LEGEND-VIVEK@users.noreply.github.com>
2022-01-04 18:21:19 +0530

    Update __init__.py

--- groupcare/modules/sql/__init__.py
+++ groupcare/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from mrjoker import DB_URI
+from groupcare import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
