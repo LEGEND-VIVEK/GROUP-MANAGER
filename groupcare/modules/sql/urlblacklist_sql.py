cc69469565805a8a8fee709c987bff0da6071135
LEGEND-VIVEK <96050472+LEGEND-VIVEK@users.noreply.github.com>
2022-01-04 19:56:31 +0530

    Update urlblacklist_sql.py

--- groupcare/modules/sql/urlblacklist_sql.py
+++ groupcare/modules/sql/urlblacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText
 
-from mrjoker.modules.sql import BASE, SESSION
+from groupcare.modules.sql import BASE, SESSION
 
 
 class URLBlackListFilters(BASE):
