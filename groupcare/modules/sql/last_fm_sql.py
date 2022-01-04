7b5ba78eae9ab54b4ff4184ecffde0deb1cf687c
LEGEND-VIVEK <96050472+LEGEND-VIVEK@users.noreply.github.com>
2022-01-04 19:42:03 +0530

    Update last_fm_sql.py

--- groupcare/modules/sql/last_fm_sql.py
+++ groupcare/modules/sql/last_fm_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String
 
-from mrjoker.modules.sql import BASE, SESSION
+from groupcare.modules.sql import BASE, SESSION
 
 
 class LastFMUsers(BASE):
