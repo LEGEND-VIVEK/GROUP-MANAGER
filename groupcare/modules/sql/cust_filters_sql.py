5bca86d5706f2e8ed96160e3932da51848263f27
LEGEND-VIVEK <96050472+LEGEND-VIVEK@users.noreply.github.com>
2022-01-04 18:57:48 +0530

    Update cust_filters_sql.py

--- groupcare/modules/sql/cust_filters_sql.py
+++ groupcare/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from mrjoker.modules.helper_funcs.msg_types import Types
-from mrjoker.modules.sql import BASE, SESSION
+from groupcare.modules.helper_funcs.msg_types import Types
+from groupcare.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
