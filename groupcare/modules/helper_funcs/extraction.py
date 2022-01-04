123269b6b2f6556e0fe40bbdf2d2afb6ee5580f0
LEGEND-VIVEK <96050472+LEGEND-VIVEK@users.noreply.github.com>
2022-01-04 14:07:40 +0530

    Update extraction.py

--- groupcare/modules/helper_funcs/extraction.py
+++ groupcare/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from mrjoker import LOGGER
-from mrjoker.modules.user import get_user_id
+from groupcare import LOGGER
+from groupcare.modules.user import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
