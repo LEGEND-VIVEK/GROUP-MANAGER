26d836e90d48fe74fd759bdcd432db57f48a38a6
LEGEND-VIVEK <96050472+LEGEND-VIVEK@users.noreply.github.com>
2022-01-06 20:03:25 +0530

    Update heroku.py

--- groupcare/modules/heroku.py
+++ groupcare/modules/heroku.py
@@ -5,8 +5,8 @@ import os
 import heroku3
 import requests
 
-from mrjoker import HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
-from mrjoker.events import register
+from groupcare import HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
+from groupcare.events import register
 
 heroku_api = "https://api.heroku.com"
 Heroku = heroku3.from_key(HEROKU_API_KEY)
